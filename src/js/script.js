a8b228e0c682f9a727e5db5230d7c97f17099cf8
Daniel Maestre <dmaestre@cifpfbmoll.eu>
2022-01-17 20:42:04 +0100

    Grid hecho, cards, y otros cambios más pequeños

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,4 @@
-console.log("Estructura de proyecto");
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
